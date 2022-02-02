1b932147e70ebffd89d812d18f6252e0da341e4b
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-02-02 14:46:25 +0100

    react-router-dom implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { About, Error, Home, SingleCocktail } from './pages';
+
 const App: React.FC = () => {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/cocktail/:id' element={<SingleCocktail />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
