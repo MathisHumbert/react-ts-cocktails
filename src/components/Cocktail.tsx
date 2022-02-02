a9d2323e40408760824f3569a600f7c6cb89520e
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-02-02 14:37:49 +0100

    set up is done

---
+++ src/components/Cocktail.tsx
@@ -0,0 +1,5 @@
+const Cocktail = () => {
+  return <div></div>;
+};
+
+export default Cocktail;
---
+++ src/components/CocktailList.tsx
@@ -0,0 +1,5 @@
+const CocktailList = () => {
+  return <div></div>;
+};
+
+export default CocktailList;
