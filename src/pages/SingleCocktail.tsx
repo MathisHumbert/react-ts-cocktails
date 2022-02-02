bdbc815c6e93aaee6d50f1a90cf0d061eabb661d
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-02-02 17:45:36 +0100

    single cocktail page done

--- src/pages/SingleCocktail.tsx
+++ src/pages/SingleCocktail.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { Loading } from '../components';
 import { useActions } from '../hooks/useActions';
@@ -15,11 +16,51 @@ const SingleCocktail: React.FC = () => {
     fetchData(`${url}${id}`);
   }, [id]);
 
-  if (loading) {
+  if (loading || data.length < 1) {
     return <Loading />;
   }
 
-  return <section></section>;
+  const { img, name, glass, alcoholic, instructions, ingredients, category } =
+    data[0];
+
+  return (
+    <section className='section cocktail-section'>
+      <Link to='/' className='btn btn-primary'>
+        back home
+      </Link>
+      <div className='drink'>
+        <img src={img} alt={name} />
+        <div className='drink-info'>
+          <p>
+            <span className='drink-data'>name: </span>
+            {name}
+          </p>
+          <p>
+            <span className='drink-data'>category: </span>
+            {category}
+          </p>
+          <p>
+            <span className='drink-data'>info: </span>
+            {alcoholic}
+          </p>
+          <p>
+            <span className='drink-data'>glass: </span>
+            {glass}
+          </p>
+          <p>
+            <span className='drink-data'>instructions: </span>
+            {instructions}
+          </p>
+          <p>
+            <span className='drink-data'>ingredients: </span>
+            {ingredients.map((item: string | null, index: number) => {
+              return <span key={index}>{item}</span>;
+            })}
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SingleCocktail;
